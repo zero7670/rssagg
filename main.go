32e241cb050d3159c938fd3fdcb956da3f986714
Khang Dao <zero7679@gmail.com>
2023-05-31 17:18:24 -0400

    boilerplate for http server is complete

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	
+	"log"
+	"net/http"
+	"os"
+	
+	"github.com/go-chi/cors"
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	router := chi.NewRouter()
+	
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }))
+	
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: 	":" + portString,
+	}
+	
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+}
